bbba37b6b731acdf2c49e8b97dd057227eb4e6b2
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-12-08 23:16:37 +0900

    Mapを使って例外を起こすプログラム

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,48 @@
 package org.example;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog1 = new Dog("クロ" , 6);
-        System.out.println("名前は：" + dog1.getName() + "です。");
-        System.out.println("年齢は：" + dog1.getAge() + "歳です。");
+        Map<String, String> musicalKeysMap = new HashMap<>();
+        musicalKeysMap.put("C" , "Cメジャースケールは「ハ長調」です。");
+        musicalKeysMap.put("Cm" , "Cマイナースケールは「ハ短調」です。");
+        musicalKeysMap.put("D" , "Dメジャースケールは「二長調」です。");
+        musicalKeysMap.put("Dm" , "Dマイナースケールは「二短調」です。");
+        musicalKeysMap.put("E" , "Eメジャースケールは「ホ長調」です。");
+        musicalKeysMap.put("Em" , "Eマイナースケールは「ホ短調」です。");
+        musicalKeysMap.put("F" , "Fメジャースケールは「ヘ長調」です。");
+        musicalKeysMap.put("Fm" , "Fマイナースケールは「ヘ短調」です。");
+        musicalKeysMap.put("G" , "Gメジャースケールは「ト長調」です。");
+        musicalKeysMap.put("Gm" , "Gマイナースケールは「ト短調」です。");
+        musicalKeysMap.put("A" , "Aメジャースケールは「イ長調」です。");
+        musicalKeysMap.put("Am" , "Aマイナースケールは「イ短調」です。");
+        musicalKeysMap.put("B" , "Bメジャースケールは「ロ長調」です。");
+        musicalKeysMap.put("Bm" , "Bマイナースケールは「ロ短調」です。");
 
-        Dog dog2 = new Dog("ポチ" , 3);
-        System.out.println("名前は：" + dog2.getName() + "です。");
-        System.out.println("年齢は：" + dog2.getAge() + "歳です。");
+        for (Map.Entry<String, String> entry : musicalKeysMap.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+        String targetKey = "";
+        try {
+            String japaneseDescription = getMusicalKeyDescription(musicalKeysMap, targetKey);
+            System.out.println(japaneseDescription);
+        } catch (myKeyNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String getMusicalKeyDescription(Map<String, String> japanese, String targetKey) throws myKeyNotFoundException {
+        if (japanese.containsKey(targetKey)) {
+            return japanese.get(targetKey);
+        } else {
+            throw new myKeyNotFoundException("指定された調性が見つかりません。");
+        }
+    }
+    static class myKeyNotFoundException extends Exception {
+        public myKeyNotFoundException(String message) {
+            super(message);
+        }
     }
 }
